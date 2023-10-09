f8c179225a605fa5f7e5570a392c9c86492ecb9a
Shashank V <145389250+curiousdev1@users.noreply.github.com>
2023-10-09 03:23:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillmember() {
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    member.style.display = "block";
+    skill.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+}
